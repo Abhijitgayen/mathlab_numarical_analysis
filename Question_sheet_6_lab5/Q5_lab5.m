932698071dad31f07409ee332c6996aa240467e9
Abhijit Gayen <76001287+Abhijitgayen@users.noreply.github.com>
2021-09-14 09:32:03 +0530

    Update Q5_lab5.m

--- Question_sheet_6_lab5/Q5_lab5.m
+++ Question_sheet_6_lab5/Q5_lab5.m
@@ -3,7 +3,7 @@ clear All;
 K=input("Enter k:");
 h=input("Enter h:");
 d=input("Enter d:");
-p=0.5;
+p=0.9;
 fn=@(x)(K^2*(p*tanh(x*d)*tanh(x*(h - d))+ 1) - x*K*(tanh(x*d) + tanh (x*(h - d))) + (x^2)*(1 - p)*tanh(x*d)* tanh(x*(h - d)));
 ok=1;
 while ok==1
@@ -15,6 +15,3 @@ error=input("Enter error :");
 fprintf("Number of iteration = %d root = %.9f\n ",n-3,x(n));
 ok=input("are you want to find another root ?(0/1) : ");
 end
-% root=1.199678640 and root=-1.199678640
-%k=h=d=1 x0=0.2,y0=0.3,z0=0.4
-%x0=-1,y0=-2,z0=-3
