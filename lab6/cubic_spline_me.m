8c5515290ded482981a5385932c296b120863426
Abhijit Gayen <76001287+Abhijitgayen@users.noreply.github.com>
2021-09-28 11:03:36 +0530

    Update cubic_spline_me.m

--- lab6/cubic_spline_me.m
+++ lab6/cubic_spline_me.m
@@ -5,13 +5,13 @@ n = length(x);
 h = x(2:n) - x(1:n-1);
 d = (y(2:n) - y(1:n-1))./h;
 
-lower = h(1:end-1);
+low= h(1:end-1);
 main  = 2*(h(1:end-1) + h(2:end));
-upper = h(2:end);
+up = h(2:end);
 
-T = spdiags([lower main upper], [-1 0 1], n-2, n-2);
-rhs = 6*(d(2:end)-d(1:end-1));
-m = T\rhs;
+T = spdiags([low main up], [-1 0 1], n-2, n-2);
+rh= 6*(d(2:end)-d(1:end-1));
+m = T\rh;
 
 m = [ 0; m; 0];
 
