4b5826363112989e7842ac0f9fb0e92078f8c20d
Abhijit Gayen <76001287+Abhijitgayen@users.noreply.github.com>
2021-09-28 11:05:48 +0530

    Update plot_cubic_spline_me.m

--- lab6/plot_cubic_spline_me.m
+++ lab6/plot_cubic_spline_me.m
@@ -1,10 +1,10 @@
 function plot_cubic_spline_me(x,s0,s1,s2,s3)
 
-n = length(x);
+no = length(x);
 
 inner_points = 20;
 
-for i=1:n-1
+for i=1:no-1
     xx = linspace(x(i),x(i+1),inner_points);
     xi = repmat(x(i),1,inner_points);
     yy = s0(i) + s1(i)*(xx-xi) +  s2(i)*(xx-xi).^2 + s3(i)*(xx - xi).^3;
